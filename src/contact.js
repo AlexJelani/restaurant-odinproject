8932e99e57289f0d8ea6d8795aa31d809dcf4733
Jelani Alexander <jelanialexander82@gmail.com>
2022-11-05 11:40:22 +0900

    add contact form heading

--- src/contact.js
+++ src/contact.js
@@ -1,3 +1,15 @@
 export function contact() {
+
+    const contentDiv = document.querySelector("#content");
+
+  //Remove any excisting DOM children if present
+  contentDiv.replaceChildren();
+
+  const heading = document.createElement("h1");
+  heading.style.color = "black";
+  heading.textContent = "Contact Me";
+  contentDiv.appendChild(heading);
+
+
     
 }
