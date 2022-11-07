3eb65580f64b0b653125738941ec7d59bfd5eaf2
Jelani Alexander <jelanialexander82@gmail.com>
2022-11-07 22:47:42 +0900

    added contact form

--- src/contact.js
+++ src/contact.js
@@ -1,6 +1,5 @@
 export function contact() {
-
-    const contentDiv = document.querySelector("#content");
+  const contentDiv = document.querySelector("#content");
 
   //Remove any excisting DOM children if present
   contentDiv.replaceChildren();
@@ -10,6 +9,16 @@ export function contact() {
   heading.textContent = "Contact Me";
   contentDiv.appendChild(heading);
 
-
-    
+  const contactDiv = document.createElement("div");
+  contactDiv.innerHTML = `<form action="https://formsubmit.co/jelanialexander82@gmail.com"      method="POST" >
+  <label for="name">Name</label> <input type="text" name="name" required> 
+  <input type="hidden" name="_next" value="./thankyou">
+  <!-- <input type="hidden" name="_captcha" value="false"> -->
+  <label for="email">Email:</label> <input type="email" name="email" required> 
+   <label for="message">Message</label> 
+  <textarea name="message" id="message" cols="30" rows="10" required></textarea>
+   <input type="submit" name="submit" value="submit">
+</form>
+</div>`;
+  heading.appendChild(contactDiv);
 }
